14deb97894aaefd639feb088cee0365e6961e021
dori135 <nolli77@ewha.ac.kr>
2025-07-09 15:08:28 +0900

    3. 연산 진행 후 결과값 출력

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,13 @@
 package calculator;
 
 public class Calculator {
+    int sum(int a, int b) { return a + b; }
 
+    int sub(int a, int b) { return a - b; }
+
+    int multi(int a, int b) { return a * b; }
+
+    int div(int a, int b) { return a / b; }
+
+    // int mod(int a, int b) { return a % b; } 사칙연산: 덧셈, 뺄셈, 곱셈, 나눗셈
 }
