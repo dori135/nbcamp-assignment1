945ea188f786c92e59649091b3fdce4604067cee
dori135 <nolli77@ewha.ac.kr>
2025-07-10 16:10:07 +0900

    2-7. 원의 넓이 계산

--- src/main/java/level2/App.java
+++ src/main/java/level2/App.java
@@ -9,33 +9,56 @@ public class App {
         String ch = ""; // 초기화
 
         do { // 무조건 1번은 실행 되도록 do while 문 사용
+            System.out.println("어떤 기능을 사용하고 싶은지 입력하세요 (사칙연산(1), 원의 넓이(2)): ");
+            int choice = sc.nextInt();
+            if (choice == 1) {
+                // 양의 정수 0포함 2개 전달 받기, 음의 정수, 실수 등 입력하지 않도록 print에 추가 설명
+                System.out.print("첫 번째 숫자(0 이상의 정수)를 입력하세요:  ");
+                int a = sc.nextInt();
+                System.out.print("두 번째 숫자(0 이상의 정수)를 입력하세요:  ");
+                int b = sc.nextInt();
 
-            // 양의 정수 0포함 2개 전달 받기, 음의 정수, 실수 등 입력하지 않도록 print에 추가 설명
-            System.out.print("첫 번째 숫자(0 이상의 정수)를 입력하세요:  ");
-            int a = sc.nextInt();
-            System.out.print("두 번째 숫자(0 이상의 정수)를 입력하세요:  ");
-            int b = sc.nextInt();
+                // 내가 가정한 사칙연산 기호와 사용자가 실제 입력하는 사칙연산 기호가 다르지 않도록 print에 추가 설명
+                System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
+                char c = sc.next().charAt(0);
 
-            // 내가 가정한 사칙연산 기호와 사용자가 실제 입력하는 사칙연산 기호가 다르지 않도록 print에 추가 설명
-            System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
-            char c = sc.next().charAt(0);
+                try {
+                    int result = calculator.calculate(a, b, c);
+                    System.out.println("결과: " + result);
+                }
+                catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    continue;
+                }
 
-            try {
-                int result = calculator.calculate(a, b, c);
-                System.out.println("결과: " + result);
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String rem = sc.next();
+                if (rem.equals("remove")) calculator.removeResult(); // 삭제 메서드
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inq = sc.next();
+                if (inq.equals("inquiry")) calculator.inquiryResults(); // 조회 메서드
             }
-            catch (Exception e) {
-                System.out.println(e.getMessage());
+            else if (choice == 2) {
+                // 양의 정수 0포함 2개 전달 받기, 음의 정수, 실수 등 입력하지 않도록 print에 추가 설명
+                System.out.print("원의 반지름(0 이상의 정수)을 입력하세요:  ");
+                int r = sc.nextInt();
+
+                double result = calculator.calculateCircleArea(r);
+                System.out.println("결과: " + result);
+
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String rem = sc.next();
+                if (rem.equals("remove")) calculator.removeAreas(); // 삭제 메서드
+
+                System.out.println("저장된 원의 넓이를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inq = sc.next();
+                if (inq.equals("inquiry")) calculator.inquiryAreas(); // 조회 메서드
+            }
+            else{
+                System.out.println("올바르지 않은 선택입니다. 1, 2 중 골라주세요.");
                 continue;
             }
-
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String rem = sc.next();
-            if (rem.equals("remove")) calculator.removeResult(); // 삭제 메서드
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String inq = sc.next();
-            if (inq.equals("inquiry")) calculator.inquiryResults(); // 조회 메서드
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             ch = sc.next();
--- src/main/java/level2/Calculator.java
+++ src/main/java/level2/Calculator.java
@@ -2,10 +2,18 @@ package level2;
 import java.util.ArrayList;
 
 public class Calculator {
+    final static double PI = 3.14159; // 변하지 않는 값, 상수 => 따라서 final static 사용
 
     // 연산 결과 저장하는 컬렉션 타입 필드, 직접 접근 못하도록 막기(private)
-    private ArrayList<Integer> arr = new ArrayList<>();
+    private ArrayList<Integer> arr1; // 사칙연산
+    private ArrayList<Double> arr2; // 원의 넓이
+    // 생성자
+    public Calculator() {
+        this.arr1 = new ArrayList<>();
+        this.arr2 = new ArrayList<>();
+    }
 
+    // 사칙연산
     // 양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산 기능 수행 후 결과 값 반환
     public int calculate(int a, int b, char c) throws DivideZeroException, IllegalOperatorException { //예외 처리
         int result = 0;
@@ -20,29 +28,58 @@ public class Calculator {
         } else {
             throw new IllegalOperatorException("올바르지 않은 연산 기호가 들어 왔습니다. (+, -, *, /)");
         }
-        this.arr.add(result);
+        this.arr1.add(result);
         return result;
     }
 
     public void inquiryResults() {
         System.out.print("저장된 연산결과 조회:  ");
-        for (Integer num : this.arr) {
+        for (Integer num : this.arr1) {
             System.out.print(num + " ");
         }
         System.out.println();
     }
 
     public void removeResult() {
-        this.arr.remove(0); // 외부에서 접근하는 게 아닌, 내부에서 접근
+        this.arr1.remove(0); // 외부에서 접근하는 게 아닌, 내부에서 접근
     }
 
     // getter
     public ArrayList<Integer> getArr() {
-        return new ArrayList<>(this.arr); // ArrayList는 참조 타입으로, 바로 반환하면 주소를 공유하는 것이기에 외부에서도 값이 수정 가능(직접 접근), 따라서 새로 만들어 내보내기
+        return new ArrayList<>(this.arr1); // ArrayList는 참조 타입으로, 바로 반환하면 주소를 공유하는 것이기에 외부에서도 값이 수정 가능(직접 접근), 따라서 새로 만들어 내보내기
     }
     // setter
     public void setArr(ArrayList<Integer> arr) {
-        this.arr = new ArrayList<>(arr); // 주소가 공유되면 외부 arr가 변경될 때 내부 arr까지 같이 변경되어 버림, 즉 캡슐화 위반. 따라서 새로 만들어서 arr에 넣어 줌
+        this.arr1 = new ArrayList<>(arr); // 주소가 공유되면 외부 arr가 변경될 때 내부 arr까지 같이 변경되어 버림, 즉 캡슐화 위반. 따라서 새로 만들어서 arr에 넣어 줌
     }
+
+    // 원의 넓이
+    public double calculateCircleArea(int r) {
+        double area = PI * r * r;
+        this.arr2.add(area); // 계산된 원의 넓이 저장
+        return area;
+    }
+
+    public void inquiryAreas() {
+        System.out.print("저장된 원의 넓이 조회:  ");
+        for (Double num : this.arr2) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public void removeAreas() {
+        this.arr2.remove(0); // 외부에서 접근하는 게 아닌, 내부에서 접근
+    }
+
+    // getter
+    public ArrayList<Double> getArr2() {
+        return new ArrayList<>(this.arr2); // ArrayList는 참조 타입으로, 바로 반환하면 주소를 공유하는 것이기에 외부에서도 값이 수정 가능(직접 접근), 따라서 새로 만들어 내보내기
+    }
+    // setter
+    public void  setArr2(ArrayList<Double> arr) {
+        this.arr2 = new ArrayList<>(arr); // 주소가 공유되면 외부 arr가 변경될 때 내부 arr까지 같이 변경되어 버림, 즉 캡슐화 위반. 따라서 새로 만들어서 arr에 넣어 줌
+    }
+
 }
 
